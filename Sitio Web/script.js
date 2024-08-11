f5f3bbf5476016b5c7c3975f1d008ced018202ed
carlostomi <carlos.tomi.chanta@vallegrande.edu.pe>
2024-08-11 03:58:02 -0500

    Publicar el código fuente de la página web

---
+++ Sitio Web/script.js
@@ -0,0 +1,11 @@
+document.getElementById('btnNosotros').addEventListener('click', function() {
+    alert('Has hecho clic en Nosotros');
+});
+
+document.getElementById('btnHistoria').addEventListener('click', function() {
+    alert('Has hecho clic en Historia');
+});
+
+document.getElementById('btnVideos').addEventListener('click', function() {
+    alert('Has hecho clic en Videos');
+});
